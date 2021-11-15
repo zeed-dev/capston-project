4d5dba5b70518373a958f583e1c6a15887a4e0a5
MUHAMMAD ZIAD AFLIAN <muhammadziadalfian07@gmail.com>
2021-11-15 20:41:04 +0800

    Edit Bookmark Page

--- lib/persentation/pages/bookmark_page.dart
+++ lib/persentation/pages/bookmark_page.dart
@@ -8,7 +8,7 @@ class BookmarkPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text("Bookmark Page"),
+        child: Text("Ini Halaman bookmark Page"),
       ),
     );
   }
